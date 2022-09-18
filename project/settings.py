6a1660963d0ce123651dcc4dffd215865b9f3fda
Javohir <javoxirone@gmail.com>
2022-09-18 21:06:44 +0500

    changed config

--- project/settings.py
+++ project/settings.py
@@ -146,3 +146,4 @@ MEDIA_URL = MEDIA_HOST + '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DISABLE_COLLECTSTATIC=1
